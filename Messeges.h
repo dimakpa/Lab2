79dff8f19f3a875f39e4617ef54cb59cc83a6311
dimakpa <70847277+dimakpa@users.noreply.github.com>
2021-09-21 22:55:12 +0300

    Create Messeges.h

---
+++ Messeges.h
@@ -0,0 +1,29 @@
+namespace Lab2 {
+
+    void messege_start(class Epycycloid *&ep);
+
+    int dialog(const char *msgs[], int N);
+
+    int D_Add_Epycycloid(Epycycloid *&ep);
+
+    int D_Set_Rad_Big(Epycycloid *&ep);
+
+    int D_Set_Rad_Small(Epycycloid *&ep);
+
+    int D_Set_Distance(Epycycloid *&ep);
+
+    int D_Get_Rad_Big(Epycycloid *&ep);
+
+    int D_Get_Rad_Small(Epycycloid *&ep);
+
+    int D_Get_Distance(Epycycloid *&ep);
+
+    int D_Point_Of_Angle(Epycycloid *&ep);
+
+    int D_Get_Curve_rad(Epycycloid *&ep);
+
+    int D_Type_Of_Epycycloid(Epycycloid *&ep);
+
+    int D_Sectorial_Square(Epycycloid *&ep);
+
+}
