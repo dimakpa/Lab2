88ef5f89a5718ce7b3a363fc93f712edf80cfe83
dimakpa <70847277+dimakpa@users.noreply.github.com>
2021-09-21 22:53:02 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Epycycloid.h"
+#include "Messege.h"
+
+
+
+int main() {
+
+    Lab2::Epycycloid *ep;
+    ep = new Lab2::Epycycloid;
+
+    messege_start(ep);
+
+    return 0;
+}
